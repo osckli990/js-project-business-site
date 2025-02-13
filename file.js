faf25fdd49f35d84e0d4dd7204edb59326b37b22
Oscar Liljefors <oscar.lf@live.se>
2025-02-13 21:08:41 +0100

    added simple validation to form, fixed some html code, fixed some css code

--- file.js
+++ file.js
@@ -1,3 +1,43 @@
+//declaring variables. (with a constant?)
+const email = document.getElementById("email");
+const form = document.getElementById('form');
+const correct = false
+
+//add a listener to prevent default submit before validation
+form.addEventListener("submit", event => {
+
+  validateInputs();
+
+  if (correct === false) {
+    event.preventDefault();
+  }
+  else {
+    NaN;
+  }
+
+});
+
+//checks for valid email adress using const i found. Returns email i lowercase
+const isValidEmail = email => {
+
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+//the validate function. .trim removes whitespaces and shortens the value. ! = not. 
+const validateInputs = () => {
+
+  const emailValue = email.value.trim();
+
+  if (!isValidEmail(emailValue)) {
+    alert("not valid");
+  }
+
+  else {
+    correct = true
+  }
+};
+
 // Get the div, and when the user clicks on it, execute myFunction
 document.getElementById("hidden-ham").onclick = function () { hamFunction() };
 
@@ -8,6 +48,10 @@ function hamFunction() {
   document.getElementById("bar-two").classList.toggle("change");
   document.getElementById("bar-three").classList.toggle("change");
 }
+
+
+
+
 
 
 
