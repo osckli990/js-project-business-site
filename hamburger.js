053d1b57c8c85afe33d53cd43b3a2d290528e633
Oscar Liljefors <oscar.lf@live.se>
2025-02-17 12:30:55 +0100

    split js into multiple files, started adding darkmode in html and css

--- hamburger.js
+++ hamburger.js
@@ -1,35 +1,3 @@
-//declaring variables. (with a constant?)
-const email = document.getElementById("email");
-const form = document.getElementById('form');
-
-//add a listener to prevent default submit before validation
-form.addEventListener("submit", event => {
-
-  if (!validateInputs()) {
-    event.preventDefault(); // Prevent form submission if validation fails
-  }
-
-});
-
-//checks for valid email adress using const i found. Returns email i lowercase
-const isValidEmail = email => {
-
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-};
-
-//the validate function. .trim removes whitespaces and shortens the value. ! = not. 
-const validateInputs = () => {
-
-  const emailValue = email.value.trim();
-
-  if (!isValidEmail(emailValue)) {
-    alert("Not a valid email address");
-    return false;
-  }
-
-  return true; // Validation passed
-};
 
 /*
 This is the original code
