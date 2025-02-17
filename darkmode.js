73dca1e8d4375e91e487803e5dd5c74812902d5b
Oscar Liljefors <oscar.lf@live.se>
2025-02-17 13:41:03 +0100

    cintinued adding darkmode. JS completed. changing classes in CSS and trying to decide how i want the site too look

---
+++ darkmode.js
@@ -0,0 +1,19 @@
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
+
+//if darkmode is active in localstorage, enable it
+if (darkmode === "active") {
+  document.body.classList.add("darkmode");
+}
+
+//excecute on click. toggle darkmode. 
+themeSwitch.addEventListener("click", () => {
+  document.body.classList.toggle("darkmode");
+
+  if (document.body.classList.contains("darkmode")) {
+    localStorage.setItem("darkmode", "active");
+  }
+  else {
+    localStorage.setItem("darkmode", "inactive");
+  }
+});
